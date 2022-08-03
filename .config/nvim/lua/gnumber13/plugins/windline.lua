aa624f9033d9e9474740e1b93aebb26ec5c9eb04
matt <matthew.mitchell@posteo.de>
2022-08-03 02:04:35 +0200

    routinely commit

--- .config/nvim/lua/gnumber13/plugins/windline.lua
+++ .config/nvim/lua/gnumber13/plugins/windline.lua
@@ -2,8 +2,8 @@ local windline = require('windline')
 -- require('gitsigns').setup()
 
 -- ~/.local/share/nvim/site/pack/paqs/start/windline.nvim/lua/wlsample/gnumber13.lua
--- require('wlsample.gnumber13')
- require('wlsample.simple')
+ require('wlsample.gnumber13')
+--- require('wlsample.simple')
 
 windline.setup({
   statuslines = {
